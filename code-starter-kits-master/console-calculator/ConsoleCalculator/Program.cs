e9cbf300ce3c11dec32182f434ab627619a3a5e4
Ishrikant <shrikant.prasad@gmail.com>
2022-01-16 13:01:34 +0530

    <Shrikant Kulkarni code>

---
+++ code-starter-kits-master/console-calculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using BowlingBall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            while(true)
+            {
+
+                Console.Write("***** WelCome to Bowling Ball Game ****" + Environment.NewLine);               
+
+                Console.Write("***** Please press 'Enter' to Start the Game ****" + Environment.NewLine);
+
+
+                var key = Console.ReadKey(true);
+
+                if (key.KeyChar == 13)
+                {
+                    Console.Write("***** Bowling Ball Game started... ****" + Environment.NewLine + Environment.NewLine);
+
+
+                    int score = calc.SendKeystroke(key.KeyChar);
+
+                    Console.Write("***** Final Score ****" + score + Environment.NewLine );
+                    Console.ReadLine(); 
+                }
+                else
+                {
+                    //If user enters any other key 'Enter' then exit the program
+                    Console.Write(" ***** Thank you ****");
+                    System.Threading.Thread.Sleep(5000);
+                    Environment.Exit(0);
+                }
+                
+                //Console.Clear();
+                //Console.Write(calc.DisplayText);
+            }
+        }
+       
+    }
+}
